fdd315d01534e886dc169b7ad241d26656d945d7
Ariel Keselman <skariel@gmail.com>
2014-10-26 12:21:52 +0200

    Fixing test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ c = Point(0,1,0)
 t = Triangle(a,b,c)
 r = Ray(Point(0.1,0.1,1), Point(0.1,0.1,-1))
 
-@test i == intersect(r, t) == Intersection(Point(0.2,0.2,0.0), 1.02, true)
+@test intersect(r, t) == Intersection(Point(0.2,0.2,0.0), 1.02, true)
 
 # yeah, more tests are needed :)
 
